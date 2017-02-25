297f94de468539c881955abc13beaa0969c0c77d
William Tumeo <tumeowilliam@gmail.com>
2017-02-24 23:12:36 -0300

    Add base for name input

--- scripts/game_over.gd
+++ scripts/game_over.gd
@@ -4,8 +4,8 @@ var teste = "testando sistema de save/load"
 var f = File.new()
 var save = "res://teste_dados"
 onready var highscore_node = get_node("/root/highscore")
-onready var high_score_text = get_node("high_score")
-onready var score_text = get_node("score")
+onready var high_score_text = get_node("score_board/high_score")
+onready var score_text = get_node("score_board/score")
 onready var achiev = get_node("unlock")
 
 func _ready():
@@ -16,6 +16,9 @@ func _ready():
 	var high_score = highscore_node.high_score
 	high_score_text.set_text("High Score: " + str(high_score))
 	score_text.set_text("Your Score: " + str(score))
+	if score > high_score:
+		get_node("online").show()
+	
 func _on_retry_button_pressed():
 	get_tree().change_scene("res://scenes/main_scene.scn")
 
